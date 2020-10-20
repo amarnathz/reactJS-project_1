e0d33864e0166b3bda6450218d07d5d29289a206
amarnathz <nsnxnskske@gmail.com>
2020-10-20 15:20:29 +0530

    navbar

---
+++ src/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NavBar(props) {
+    return (
+        <div className= "navbar">
+
+            <span id = "logo" >Find Super hero </span>
+        </div>
+    );
+}
+
+export default NavBar;
---
+++ src/SearchResults.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchResults(props) {
+    return (
+        <div>
+            SearchResults
+        </div>
+    );
+}
+
+export default SearchResults;
